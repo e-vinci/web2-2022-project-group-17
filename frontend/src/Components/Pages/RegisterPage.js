fa4955330691075039a10ed70dd7cc3e72a6dd53
Porridjo <Den.nguyen69@gmail.com>
2022-12-17 21:57:51 +0100

    fix api request

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -222,7 +222,7 @@ function renderRegisterForm() {
       registerFormGroup1.className = "register-form-group failure";
       usernameSmall.innerHTML = "Veuillez introduire un nom d'utilisateur";
     } else {
-      const response = await fetch(`${process.env.API_BASE_URL}/api/users/${username.value}`);
+      const response = await fetch(`${process.env.API_BASE_URL}/users/${username.value}`);
       if (response.ok) {
         registerFormGroup1.className = "register-form-group failure";
         usernameSmall.innerHTML = "Nom d'utilisateur déjà existant";
