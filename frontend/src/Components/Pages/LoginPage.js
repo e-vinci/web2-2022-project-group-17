281eed60907b63cad45810983ac8b573569d6d0a
tqueguin <tanguyqueguineur@gmail.com>
2022-12-17 16:26:04 +0100

    change cors origin api

--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -146,7 +146,7 @@ async function onLogin(e) {
     }
   }
 
-  const response = await fetch('/api/auths/login', options);
+  const response = await fetch(`${process.env.API_BASE_URL}/auths/login`, options);
   if (!response.ok) {
     const errorP = document.querySelector('.login-error-p');
     errorP.classList.add('text-center');
