0003c607c34cd477ac73f61236234dc1796e1ac1
Porridjo <Den.nguyen69@gmail.com>
2022-12-18 04:43:48 +0100

    fix cors

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ const rateLimit = require('express-rate-limit')
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:8080','https://tqueguin.github.io','localhost:8080'],
+  origin: ['http://localhost:8080','https://tqueguin.github.io'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
